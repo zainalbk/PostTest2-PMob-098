e97a2e1893ad92d2f54e27cd6b70edb5d7e438e2
zainalbk <162242493+zainalbk@users.noreply.github.com>
2025-10-15 21:36:46 +0700

    File Prak2

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PostTest P.Mob_2300018098_Rabu 13.45-15.15"
+include(":app")
